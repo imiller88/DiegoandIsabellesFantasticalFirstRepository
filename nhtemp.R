7837ca3a13e2d319576056d9cf3f793d799ab427
ISABELLE <isabelle.k.miller@gmail.com>
2016-02-28 20:01:49 +0100

    updated file

--- nhtemp.R
+++ nhtemp.R
@@ -1,3 +1,17 @@
+#Data Exploration: Average Yearly Temperatures in New Haven, CT from 1912-1971#
+#open piping package#
+library(dplyr)
+#explore nhtemp#
 ?nhtemp
+#draw up nhtemp data#
 data(nhtemp)
-hist(nhtemp)
+#summary statistics#
+summary(nhtemp)
+#histogram: frequency of average yearly temperatures#
+hist(nhtemp, 
+     main="Frequency of Average Yearly Temperatures", 
+     xlab="temperature") ## ASK ABOUT THIS ERROR
+#logged histogram#
+log(nhtemp) %>% 
+  hist(main="Logged Frequency of Average Yearly Temperatures (1912-1971)",
+       xlab="temperature")
