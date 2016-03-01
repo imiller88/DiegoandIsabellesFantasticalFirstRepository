d6c5322f27a1f414f16f4a01aa965b999e3e0ea4
Diego Fernández Fernández <dgdoblecero@gmail.com>
2016-03-01 13:58:58 +0100

    Revert "Revert "More descriptive statistics""
    This reverts commit 8613386354a9aa8123e3694fdbb1a0f6c00fb3d0.

--- nhtemp.R
+++ nhtemp.R
@@ -1,3 +1,31 @@
+#Dynamic linking: source USArrests.R file
+#setting working directory (this will need to be set by user)
+getwd()
+list.files()
+setwd('/Users/isabellemiller/GitHub/DiegoandIsabellesFantasticalFirstRepository/')
+source('USArrests.R')
+
+#Data Exploration: Average Yearly Temperatures in New Haven, CT from 1912-1971#
+#open piping package#
+library(dplyr)
+#explore nhtemp#
 ?nhtemp
+#draw up nhtemp data#
 data(nhtemp)
-hist(nhtemp)
+#summary statistics#
+summary(nhtemp)
+
+#plot data by time
+plot(nhtemp,
+    main="Average Yearly Temperature 1912-1971,
+    ylab="Temperature (Fahrenheit)")
+
+#histogram: frequency of average yearly temperatures#
+hist(nhtemp, 
+     main="Frequency of Average Yearly Temperatures", 
+     xlab="temperature") ## ASK ABOUT THIS ERROR
+#logged histogram#
+log(nhtemp) %>% 
+  hist(main="Logged Frequency of Average Yearly Temperatures (1912-1971)",
+       xlab="temperature")
+
